a7ac714051408d0b7f7057ac8025a02ae9330c7e
Maclovin <m4k2005@yahoo.com>
2016-08-10 19:00:17 -0300

    Removing unecessary script.

--- Assets/Scripts/CameraSwitcherController.js.js
+++
@@ -1,9 +0,0 @@
-﻿#pragma strict
-
-function OnCollisionEnter (other : Collision) {
-	var mainCamera = GameObject.FindGameObjectsWithTag('MainCamera')[0];
-
-	if (other.gameObject.CompareTag('Player')) {
-		Debug.Log(mainCamera);
-	}
-}
